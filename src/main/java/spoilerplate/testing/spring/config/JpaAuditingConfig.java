ab82ce34f54e764bc2ae16eca2d8d2b105440b00
wbluke <pwb0127@gmail.com>
2023-02-25 16:38:08 +0900

    separate jpa auditing config from main application class

---
+++ src/main/java/spoilerplate/testing/spring/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package spoilerplate.testing.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaAuditingConfig {
+}
