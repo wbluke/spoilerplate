2ac2d2d7a7656fe1423fb5d95ce7076e1a6373e7
wbluke <pwb0127@gmail.com>
2024-02-11 16:06:54 +0900

    [Minesweeper] 깃발 꽂기 후 종료 체크 추가

--- src/main/java/spoilerplate/cleancode/minesweeper/asis/MinesweeperGame.java
+++ src/main/java/spoilerplate/cleancode/minesweeper/asis/MinesweeperGame.java
@@ -1,6 +1,5 @@
 package spoilerplate.cleancode.minesweeper.asis;
 
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,22 +14,17 @@ public class MinesweeperGame {
         System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
         System.out.println("지뢰찾기 게임 시작!");
         System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
         Scanner scanner = new Scanner(System.in);
-
-        // 게임판 초기화
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 10; j++) {
                 board[i][j] = "□";
             }
         }
-        // 지뢰 배치
         for (int i = 0; i < 10; i++) {
             int width = new Random().nextInt(10);
             int height = new Random().nextInt(8);
             mines[height][width] = true;
         }
-        // 카운트 초기화
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 10; j++) {
                 int count = 0;
@@ -65,7 +59,6 @@ public class MinesweeperGame {
                 mineCounts[i][j] = count;
             }
         }
-
         while (true) {
             System.out.println("   A B C D E F G H I J");
             for (int i = 0; i < 8; i++) {
@@ -75,27 +68,22 @@ public class MinesweeperGame {
                 }
                 System.out.println();
             }
-
             if (gameStatus == 1) {
                 System.out.println("지뢰를 모두 찾았습니다. GAME CLEAR!");
                 break;
             }
-
             if (gameStatus == -1) {
                 System.out.println("지뢰를 밟았습니다. GAME OVER!");
                 break;
             }
-
             System.out.println();
             System.out.println("오픈할 좌표를 입력하세요. (예: a1)");
             String input = scanner.nextLine();
             System.out.println(input + " 셀을 선택하셨습니다.");
             System.out.println(input + " 셀에 대한 행위를 선택하세요. (1: 오픈, 2: 깃발 꽂기)");
             String input2 = scanner.nextLine();
-
             char c = input.charAt(0);
             char r = input.charAt(1);
-
             int col;
             switch (c) {
                 case 'a':
@@ -132,15 +120,23 @@ public class MinesweeperGame {
                     col = -1;
                     break;
             }
-
             int row = Character.getNumericValue(r) - 1;
-
             if (input2.equals("2")) {
                 board[row][col] = "⚑";
                 System.out.println(input + " 셀에 깃발을 꽂았습니다.");
+                boolean isAllOpened = true;
+                for (int i = 0; i < 8; i++) {
+                    for (int j = 0; j < 10; j++) {
+                        if (board[i][j] == "□") {
+                            isAllOpened = false;
+                        }
+                    }
+                }
+                if (isAllOpened) {
+                    gameStatus = 1;
+                }
                 continue;
             }
-
             if (mines[row][col]) {
                 board[row][col] = "☼";
                 gameStatus = -1;
@@ -148,7 +144,6 @@ public class MinesweeperGame {
             } else {
                 open(row, col);
             }
-
             boolean isAllOpened = true;
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 10; j++) {
@@ -160,9 +155,7 @@ public class MinesweeperGame {
             if (isAllOpened) {
                 gameStatus = 1;
             }
-
         }
-
     }
 
     private static void open(int row, int col) {
@@ -175,15 +168,12 @@ public class MinesweeperGame {
         if (mines[row][col]) {
             return;
         }
-
         if (mineCounts[row][col] != 0) {
             board[row][col] = String.valueOf(mineCounts[row][col]);
             return;
         } else {
             board[row][col] = "■";
         }
-
-
         open(row - 1, col - 1);
         open(row - 1, col);
         open(row - 1, col + 1);
